e4b751b44c0595abfe478a3e3767cae1f71d1e2c
karti16 <kartim316@gmail.com>
2023-03-02 01:52:07 +0530

    Added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import React, { useState } from 'react';
 import NewSegments from './Components/NewSegments';
 
-const otherContentStyles = {
-  position: 'relative',
-  zIndex: 2,
-  backgroundColor: 'red',
-  padding: '10px',
-};
-
-const buttonStyles = {
-  position: 'relative',
-  zIndex: 1,
-};
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <>
       <div>Navbar</div>
-      <div
-        style={buttonStyles}
-        onClick={() => console.log('clicked')}
-        aria-hidden="true"
-      >
+      <div>
         <button type="button" onClick={() => setIsOpen(true)}>
           Save Segment
         </button>
         <NewSegments isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
-      <div style={otherContentStyles}>Other Contents</div>
     </>
   );
 }
--- src/Components/NewSegments.jsx
+++ src/Components/NewSegments.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
+import RemoveIcon from '@mui/icons-material/Remove';
 import {
   FormControl,
   InputLabel,
   MenuItem,
   Select,
   TextField,
+  IconButton,
+  Link,
+  Button,
+  Card,
+  Typography,
+  Grid,
+  Box,
 } from '@mui/material';
 
 const popupStyles = {
   position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  left: 0,
+  height: '100%',
+  // transform: 'translate(-50%, -50%)',
   backgroundColor: '#FFF',
   padding: '50px',
   zIndex: 1000,
@@ -28,70 +36,179 @@ const overlayStyles = {
   zIndex: 1000,
 };
 
-function SegmentsToAdd({ list }) {
+function SegmentsToAdd({ list, setList, removeSchema, addSchema }) {
+  const [selectedSchema, setSelectedSchema] = useState('');
   return (
-    <FormControl fullWidth>
-      <InputLabel id="demo-simple-select-label">
-        Add schema to segment
-      </InputLabel>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        // value={age}
-        label="Add schema to segment"
-        // onChange={handleChange}
-      >
-        {list.map((item) => (
-          <MenuItem value={item.value}>{item.label}</MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <>
+      <FormControl fullWidth size="small">
+        <InputLabel
+          id="demo-simple-select-label"
+          style={{ height: 30, fontSize: '12px' }}
+        >
+          Add schema to segment
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={selectedSchema}
+          label="Add schema to segment"
+          onChange={(e) => setSelectedSchema(e.target.value)}
+          style={{ height: 30, fontSize: '12px' }}
+        >
+          {list.map(
+            (item) =>
+              !item.added && (
+                <MenuItem key={item.value} value={item.value}>
+                  {item.label}
+                </MenuItem>
+              ),
+          )}
+        </Select>
+      </FormControl>
+      <Box display="flex" justifyContent="flex-start">
+        <Button
+          sx={{ height: 30, fontSize: '10px' }}
+          onClick={() => {
+            addSchema(selectedSchema);
+          }}
+        >
+          +Add new schema
+        </Button>
+      </Box>
+    </>
   );
 }
 
-function SelectedSchema({ schemaList }) {
-  return schemaList.map((item) => (
-    <TextField
-      defaultValue={item.label}
-      variant="filled"
-      InputProps={{ disableUnderline: true, readOnly: true }}
-    />
-  ));
+function SelectedSchema({ list, setList, removeSchema, addSchema }) {
+  const modifySelectedSchema = (oldValue, newValue) => {
+    const oldValueIndex = list.findIndex((i) => i.value === oldValue);
+    const newValueIndex = list.findIndex((i) => i.value === newValue);
+
+    const tempList = list;
+    tempList[oldValueIndex].added = false;
+    tempList[newValueIndex].added = true;
+    console.log(tempList);
+    setList((prevList) => [...tempList]);
+  };
+
+  return (
+    <>
+      {list.map(
+        (item) =>
+          item.added && (
+            <Grid
+              key={item.value}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <FormControl fullWidth size="small">
+                <InputLabel
+                  id="demo-simple-select-label"
+                  style={{ height: 30, fontSize: '12px' }}
+                />
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={item.value}
+                  displayEmpty
+                  style={{ height: 30, fontSize: '12px' }}
+                  label=""
+                  onChange={(e) =>
+                    modifySelectedSchema(item.value, e.target.value)
+                  }
+                >
+                  {list.map((i) => (
+                    <MenuItem key={i.value} disabled={i.added} value={i.value}>
+                      {i.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <IconButton onClick={() => removeSchema(item.value)}>
+                <RemoveIcon />
+              </IconButton>
+            </Grid>
+          ),
+      )}
+    </>
+  );
 }
 
 function NewSegments({ isOpen, setIsOpen }) {
   if (!isOpen) return null;
 
-  const [schemaList, setSchemaList] = useState([
-    { label: 'First Name', Value: 'first_name' },
-  ]);
   const [list, setList] = useState([
-    { label: 'First Name', Value: 'first_name' },
-    { label: 'Last Name', Value: 'last_name' },
-    { label: 'Gender', Value: 'gender' },
-    { label: 'Age', Value: 'age' },
-    { label: 'Account Name', Value: 'account_name' },
-    { label: 'City', Value: 'city' },
-    { label: 'State', Value: 'state' },
+    { label: 'First Name', value: 'first_name', added: false },
+    { label: 'Last Name', value: 'last_name', added: false },
+    { label: 'Gender', value: 'gender', added: false },
+    { label: 'Age', value: 'age', added: false },
+    { label: 'Account Name', value: 'account_name', added: false },
+    { label: 'City', value: 'city', added: false },
+    { label: 'State', value: 'state', added: false },
   ]);
+
+  const [addNewSchema, setAddNewSchema] = useState(false);
+
+  const addSchema = (selectedSchema) => {
+    const objIndex = list.findIndex((i) => i.value === selectedSchema);
+    const tempList = list;
+    tempList[objIndex].added = true;
+    console.log(tempList);
+    setList((prevList) => [...tempList]);
+  };
+
+  const removeSchema = (value) => {
+    console.log('ssss', value);
+    const objIndex = list.findIndex((i) => i.value === value);
+    const tempList = list;
+    tempList[objIndex].added = false;
+    console.log(tempList);
+    setList((prevList) => [...tempList]);
+  };
+
   return ReactDom.createPortal(
     <div style={overlayStyles}>
-      <div style={popupStyles}>
-        New Segments
-        <div>
-          <TextField placeholder="Name of the segment" />
-        </div>
-        <p>
-          To save your segment, you need to add the schemas to build the query
-        </p>
-        <SelectedSchema schemaList={schemaList} />
-        <SegmentsToAdd list={list} />
-        <div>
-          <button type="button" onClick={() => setIsOpen(false)}>
-            close
-          </button>
-        </div>
-      </div>
+      <Card style={popupStyles}>
+        <Grid display="flex" flexDirection="column" gap="10px">
+          <Typography variant="h9">Enter the Name of the Segment</Typography>
+
+          <TextField placeholder="Name of the segment" size="small" />
+          <Typography variant="h9">
+            To save your segment, you need to add the schemas to build the query
+          </Typography>
+          <SelectedSchema
+            list={list}
+            setList={setList}
+            addSchema={addSchema}
+            removeSchema={removeSchema}
+          />
+          <SegmentsToAdd
+            list={list}
+            setList={setList}
+            addSchema={addSchema}
+            removeSchema={removeSchema}
+          />
+          <Grid display="flex" gap="10px">
+            <Button
+              type="button"
+              variant="contained"
+              color="success"
+              onClick={() => console.log('save')}
+            >
+              Submit
+            </Button>
+            <Button
+              type="button"
+              variant="outlined"
+              color="error"
+              onClick={() => setIsOpen(false)}
+            >
+              close
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
     </div>,
     document.getElementById('portal'),
   );
@@ -103,11 +220,29 @@ NewSegments.propTypes = {
 };
 
 SegmentsToAdd.propTypes = {
-  list: PropTypes.arrayOf.isRequired,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      added: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  setList: PropTypes.func.isRequired,
+  addSchema: PropTypes.func.isRequired,
+  removeSchema: PropTypes.func.isRequired,
 };
 
 SelectedSchema.propTypes = {
-  schemaList: PropTypes.arrayOf.isRequired,
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      added: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+  setList: PropTypes.func.isRequired,
+  addSchema: PropTypes.func.isRequired,
+  removeSchema: PropTypes.func.isRequired,
 };
 
 export default NewSegments;
