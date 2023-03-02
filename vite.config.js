99540dd22942f3abcac67361cc873c99884e11bc
karti16 <kartim316@gmail.com>
2023-03-02 21:27:52 +0530

    added ghpages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  base: '/customerLabs-frontend',
   server: {
     port: 3000,
     host: '0.0.0.0',
